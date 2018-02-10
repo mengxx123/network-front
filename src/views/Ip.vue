1cc611afb2aea1a7e700a27afb8e786c93cc8987
cjh <1418503647@qq.com>
2018-02-10 23:10:05 +0800

    update IP tool

--- src/views/Ip.vue
+++ src/views/Ip.vue
@@ -1,7 +1,16 @@
 <template>
     <my-page title="IP 查询" :page="page">
         <div class="card">
-            本机 IP： <span class="strong">{{ ip }}</span>
+            你的 IP： <span class="strong">{{ ip }}</span>
+        </div>
+        <div>
+            <ui-text-field v-model="ip" label="IP 地址" hintText="输入 IP 地址" />
+            <br>
+            <ui-raised-button label="查询" primary @click="query" />
+        </div>
+        <div class="result" v-if="detail">
+            <div>所在地区：{{ detail.country }} {{ detail.region }} {{ detail.city }}</div>
+            <div>{{ detail.isp }}</div>
         </div>
     </my-page>
 </template>
@@ -11,6 +20,7 @@
         data () {
             return {
                 ip: '加载中...',
+                detail: null,
                 page: {
                     menu: [
                         {
@@ -31,10 +41,25 @@
                     response => {
                         let data = response.data
                         this.ip = data
+                        this.getDetail(this.ip)
                     },
                     response => {
                         console.log(response)
                     })
+            },
+            getDetail(ip) {
+                this.$http.get('http://node.api.yunser.com/ip?ip=' + ip).then(
+                    response => {
+                        let data = response.data
+                        console.log(data)
+                        this.detail = data
+                    },
+                    response => {
+                        console.log(response)
+                    })
+            },
+            query() {
+                this.getDetail(this.ip)
             }
         }
     }
@@ -43,11 +68,15 @@
 <style lang="scss" scoped>
     .card {
         width: 320px;
+        margin-bottom: 16px;
         padding: 16px;
         box-shadow: 0 1px 6px rgba(0,0,0,.117647), 0 1px 4px rgba(0,0,0,.117647);
         .strong {
             color: #ff4081;
             font-size: 24px;
         }
+    }
+    .result {
+        margin-top: 16px;
     }
 </style>
